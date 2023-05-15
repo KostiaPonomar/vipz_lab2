cc9e566bbc5c440abb6e7d370e9a48853c1a6c9a
Kostia <95354876+KostiaPonomar@users.noreply.github.com>
2023-05-15 10:09:42 +0300

    Update prog.h

--- prog.h
+++ prog.h
@@ -1,24 +1,24 @@
 #pragma once
 #include <string>
 
-class CStudent
+struct SStudent
 {
 public:
-    char m_cName[50];
-    char m_cSecondName[50];
-    char m_cdob[12];
-    int mark[6];
-    double m_davg;
-    CStudent* pnext;
+    char m_cName[50];//поле для задання імені студента
+    char m_cSecondName[50];//поле для задання прізвище студента
+    char m_cdob[12];//поле для задання
+    int mark[6];//масив оцінок
+    double m_davg;//середнє арифметичне
+    SStudent* pnext;//вказівник на наступний елемент зв'язного списку
 };
-CStudent* SearchAndDel(CStudent *p, int* size);
-void ReadFromFile(CStudent*& p, CStudent*& head, int& size);
-void ReadFromConsole(CStudent*& p, CStudent*& head, int& size);
-void RecordingToFile(CStudent*& p, CStudent*& head);
-void Display(CStudent* p, CStudent* head);
-void DisplayAllExcept5(CStudent* p, CStudent* head);
-void SortList(CStudent** head, int size);
-void OldestStudent(CStudent**head);
-void YoungestStudent(CStudent **head);
-void InsertNewStudent(CStudent* prev_node, CStudent** head, int& size);
-void DeleteFromPosition(CStudent** head, int& size);
+SStudent* SearchAndDel(SStudent *p, int* size);//видалення за умовою
+void ReadFromFile(SStudent*& p, SStudent*& head, int& size);//зчитування з файлу
+void ReadFromConsole(SStudent*& p, SStudent*& head, int& size);//зчитування з консолі
+void RecordingToFile(SStudent*& p, SStudent*& head);//запис зв'язного списку у файл
+void Display(SStudent* p, SStudent* head);//вивід зв'язного списку
+void DisplayAllExcept5(SStudent* p, SStudent* head);//вивід студентів котрі не мають оцінки ні однієї оцінки 5
+void SortList(SStudent** head, int size);//сортування списку студентів
+void OldestStudent(SStudent**head);//вивід найстоаршого
+void YoungestStudent(SStudent **head);//вивід наймолодшого
+void InsertNewStudent(SStudent* prev_node, SStudent** head, int& size);//додавання нового студента у задану позицію списку
+void DeleteFromPosition(SStudent** head, int& size);//видалення студента з заданної позиції списку
